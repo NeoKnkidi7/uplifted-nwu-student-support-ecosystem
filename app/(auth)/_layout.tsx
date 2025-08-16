cd0de914705e0e6838ba694e5eeb4b9bfb1e5bbe
Neo K. Nkidi <mneva7neo@gmail.com>
2025-08-16 15:03:56 +0200

    Update _layout.tsx

--- app/(auth)/_layout.tsx
+++ app/(auth)/_layout.tsx
@@ -1,1 +1,12 @@
-export default function AuthLayout() { return null }
+import { Stack } from "expo-router";
+import React from "react";
+
+export default function AuthLayout() {
+  return (
+    <Stack screenOptions={{ headerShown: false }}>
+      <Stack.Screen name="index" />
+      <Stack.Screen name="login" />
+      <Stack.Screen name="register" />
+    </Stack>
+  );
+}
